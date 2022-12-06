5814d17552fd5aad50de051949dc2d86184ffebe
MaedaKenji <66903425+MaedaKenji@users.noreply.github.com>
2022-12-06 20:27:58 +0700

    Update main.c
    Mungkin lvl 4

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int x,y;
 int xx,yy;
 int i,j;
 int sum=0;
+int CD;
 
 
 int checkWin()
@@ -74,6 +75,8 @@ void printD()
     int CW= checkWin();
     if (CW==1) {  printf("Player 1 Win !!\n");}
     else if (CW==2) {  printf("Player 2 Win !!\n");}
+    CD= checkDraw();
+    if (CD==1) printf("Draw !!\n");
 
 }
 
@@ -83,25 +86,114 @@ void input()
     int cek=0;
 
     printf("Player 1 your move: "); scanf("%d %d",&x,&y);
-    if (layout[x-1][y-1]==0) {layout[x-1][y-1]=1;}
+    if (layout[x-1][y-1]==0 && x<4 && y<4) {layout[x-1][y-1]=1;}
     else
         while (cek!=1)
-    {
-        printf("Tidak bisa !!\n");
-        printf("Player 1 your move: \n");
-        scanf("%d %d",&x,&y);
-        if (layout[x-1][y-1]==0) {  layout[x-1][y-1]=1; cek=1;}
-    }
+            {
+                printf("Tidak bisa !!\n");
+                printf("Player 1 your move: \n");
+                scanf("%d %d",&x,&y);
+                if (layout[x-1][y-1]==0 && x<4 && y<4) {  layout[x-1][y-1]=1; cek=1;}
+            }
 
     int CW= checkWin();
     if (CW==1 || CW ==2 ) return;
-    int CD= checkDraw();
-    if (CD==1) {    printf("Draw\n"); return;}
 }
 
 
 void computer()
 {   if (layout[1][1]==0) {  layout[1][1]=-1;    return;}
+
+    // Untuk menang Diagonal Kiri
+    if (layout[2][0]+layout[1][1] == -2  && layout[0][2]==0) {  layout [0][2]=-1;    return;}
+    if (layout[2][0]+layout[0][2] == -2  && layout[1][1]==0) {  layout [1][1]=-1;    return;}
+    if (layout[1][1]+layout[0][2] == -2  && layout[2][0]==0) {  layout [2][0]=-1;    return;}
+
+    // Untuk Menang Diagonal Kanan
+    if (layout[0][0]+layout[1][1] == -2 && layout[2][2]==0) {  layout [2][2]=-1;   return;}
+    if (layout[0][0]+layout[2][2] == -2 && layout[1][1]==0) {  layout [1][1]=-1;  return;}
+    if (layout[1][1]+layout[2][2] == -2 && layout[0][0]==0) {  layout [0][0]=-1;   return;}
+
+    // block Diagonal Kanan
+    if (layout[0][0]+layout[1][1] == 2 && layout[2][2]==0) {  layout [2][2]=-1;   return;}
+    if (layout[0][0]+layout[2][2] == 2 && layout[1][1]==0) {  layout [1][1]=-1;  return;}
+    if (layout[1][1]+layout[2][2] == 2 && layout[0][0]==0) {  layout [0][0]=-1;   return;}
+
+
+    // block Diagonal Kiri
+    if (layout[2][0]+layout[1][1] == 2  && layout[0][2]==0) {  layout [0][2]=-1;    return;}
+    if (layout[2][0]+layout[0][2] == 2  && layout[1][1]==0) {  layout [1][1]=-1;    return;}
+    if (layout[1][1]+layout[0][2] == 2  && layout[2][0]==0) {  layout [2][0]=-1;    return;}
+
+
+    int sum;
+    // Win Kolom
+    for(int i=0;i<3;i++)
+    {   sum=0;
+        for (int j=0;j<3;j++)
+            {   sum+= layout[j][i];
+                if (sum== -2)
+                {   for (int k=0;k<3;k++)
+                        {   if (layout [k][i]==0)
+                                {   layout[k][i]=-1;
+                                    return;
+                                }
+
+                        }
+                }
+            }
+    }
+
+    // Win Baris
+    for(int i=0;i<3;i++)
+    {   sum=0;
+        for (int j=0;j<3;j++)
+            {   sum+= layout[i][j];
+                if (sum== -2)
+                {   for (int k=0;k<3;k++)
+                        {   if (layout [i][k]==0)
+                                {   layout[i][k]=-1;
+                                    return;
+                                }
+
+                        }
+                }
+            }
+    }
+
+    // block Kolom
+    for(int i=0;i<3;i++)
+    {   sum=0;
+        for (int j=0;j<3;j++)
+            {   sum+= layout[j][i];
+                if (sum== 2)
+                {   for (int k=0;k<3;k++)
+                        {   if (layout [k][i]==0)
+                                {   layout[k][i]=-1;
+                                    return;
+                                }
+
+                        }
+                }
+            }
+    }
+
+    // block Baris
+    for(int i=0;i<3;i++)
+    {   sum=0;
+        for (int j=0;j<3;j++)
+            {   sum+= layout[i][j];
+                if (sum== 2)
+                {   for (int k=0;k<3;k++)
+                        {   if (layout [i][k]==0)
+                                {   layout[i][k]=-1;
+                                    return;
+                                }
+
+                        }
+                }
+            }
+    }
 
 
 
@@ -132,17 +224,17 @@ int main()
     while (win!=1 || win != 2)
     {
         input();
+        printD();
         win=checkWin();
         CD=checkDraw();
-        printD();
         if (win==1 || win == 2) break;
         if (CD==1) break;
         printf("\nComputer Move\n");
 
         computer();
+        printD();
         win=checkWin();
         CD=checkDraw();
-        printD();
         if (win==1 || win== 2) break;
         if (CD==1) break;
         printf("\n\n");
